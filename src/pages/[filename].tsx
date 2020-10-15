26d8b0b63e77dd346da35469baff8ed4abcda274
Lyuzhi Pan <panlvzhi@foxmail.com>
2020-10-15 22:13:42 +0800

    Update [filename].tsx

--- src/pages/[filename].tsx
+++ src/pages/[filename].tsx
@@ -3,10 +3,12 @@ import downloadlist from '../../downloadlist.json';
 
 export default (props:any) => {
 	const { filename } = props.match.params;
+  let message = "404 Not Found.";
 	if (downloadlist[filename]) {
 		window.location.href = downloadlist[filename];
+    message = "downloading";
 	}	
 	return (<div>
-		404 Not Found.
+      {message}
 	</div>);
 }
